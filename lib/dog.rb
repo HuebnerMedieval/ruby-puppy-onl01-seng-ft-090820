bf26160ffaef5becad6838b8a67b77efde5b7b4f
Eli Huebner <elimhuebner@gmail.com>
2020-09-16 13:14:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,21 @@ class Dog
   
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      
+    end
   end
   
 end
